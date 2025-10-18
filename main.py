f556975ce9b632fedce256254f18150ed620e7f6
Hüseyin Can Gündüz <hcangunduz@gmail.com>
2025-10-18 23:46:28 +0300

    some library study and some standarts for this repo

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-import sys
-print("Hello world")
-print("Working at " + ','.join(sys.path))
+import aiohttp
+import asyncio
+
+async def main():
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get('http://lotus.tetakent.com') as response:
+
+            print("Status:", response.status)
+            print("Content-type:", response.headers['content-type'])
+
+            html = await response.text()
+            print("Body:", html[:100], "...")
+
+asyncio.run(main())
